6227a16cf71d32a24edaed6e2d0c227da0fc18cf
deepthina <73054651+deepthina@users.noreply.github.com>
2022-03-23 12:37:17 +0530

    Add files via upload

---
+++ components/BottomTabNavigator.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import Search from "../screens/Search";
+import Transaction from "../screens/Transaction";
+
+
+
+export default class BottomTabNavigator extends React.Component {
+  render() {
+    return (
+    
+    );
+  }
+}
